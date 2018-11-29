03654b6dc5ee1a5f21ad544dd4f19d8ebc44a4c3
Kazzpa <andres97cb@hotmail.com>
2018-11-29 21:04:53 +0100

    Commit from netbeans

--- EPD7/p1/index.php
+++ EPD7/p1/index.php
@@ -10,25 +10,48 @@ and open the template in the editor.
         <title>EPD7-P1</title>
         <script type="text/javascript">
 //<!--
-            function trata(str, cad) {
-                i = str.indexOf("(");
-                j = str.indexOf(",");
-                k = str.indexOf(")");
-                if (i < 0 || k < 0) {
+            function valida(str) {
+                ini = str.indexOf("(");
+                mid = str.indexOf(",");
+                last = str.indexOf(")");
+                if (ini < 0 || last < 0) {
                     document.write("Invalido el comando");
                     return -1;
                 }
                 //OP
-                var op = str.substring(0, i);
+                var op = str.substring(0, ini);
 
-                var a = str.substring(i + 1, j);
+                var a = str.substring(ini + 1, mid);
                 //si hay 2 atributos
-                if (j >= 0) {
-                    var b = str.substring(j + 1, k);
+                var res;
+                if (mid != -1) {
+                    var b = str.substring(mid + 1, last);
+                    res = [op,a,b];
+                }else{
+                    res = [op,a];
+                }
+                return res;
+                
+            }
+            function trata(str, cad) {
+                ini = str.indexOf("(");
+                mid = str.indexOf(",");
+                last = str.indexOf(")");
+                if (ini < 0 || last < 0) {
+                    document.write("Invalido el comando");
+                    return -1;
+                }
+                //OP
+                var op = str.substring(0, ini);
+
+                var a = str.substring(ini + 1, mid);
+                //si hay 2 atributos
+                if (mid >= 0) {
+                    var b = str.substring(mid + 1, last);
                 }
                 switch (op) {
                     //devuelve subcadena de cad desde  i a j
-                    case sub:
+                    case "sub":
                         inta = parseInt(a);
                         intb = parseInt(b);
                         //comprobamos que sean numeros
@@ -36,28 +59,28 @@ and open the template in the editor.
                             if (inta > intb || intb > cad.length || inta < 0) {
                                 return -1;
                             }
-                        }else{
+                        } else {
                             return -1;
                         }
-                        return cad.substring(inta,intb);
-                        
+                        return cad.substring(inta, intb + 1);
+
 
                         break
                         //unico parametro
                         //concatena a al final de cad
-                    case cat:
-                        if (j >= 0) {
+                    case "cat":
+                        if (mid >= 0) {
                             return -1;
                         }
                         //TRABAJA
                         return cad.concat(a);
                         break;
                         //remplaza en cad a por b
-                    case rep:
-                        return cad.replace(a,b);
+                    case "rep":
+                        return cad.replace(a, b);
                         break;
                     default:
-                        document.write("No valida op");
+                        return "No valida op";
                         break;
                 }
             }
@@ -72,7 +95,17 @@ and open the template in the editor.
             var cadArr = cadena.split(";");
             var cad = cadArr[0];
             for (var i = 1; i < cadArr.length; i++) {
-                trata(cadArr[i], cad);
+                operandos = valida(cadArr[i]);
+                var res = trata(cadArr[i], cad);
+                if (res == -1) {
+                    alert("error");
+                } else {
+                    res.concat("<br>");
+                    document.write(res);
+                }
+            }
+            if (cadArr.length < 2) {
+                alert("introduzca mas campos");
             }
 
 //-->

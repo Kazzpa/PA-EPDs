108912925c81d185869e05a622075ba7fde2d95b
Kazzpa <andres97cb@hotmail.com>
2018-11-30 13:34:53 +0100

    Finalizado p1

--- EPD7/p1/index.php
+++ EPD7/p1/index.php
@@ -11,9 +11,9 @@ and open the template in the editor.
         <script type="text/javascript">
 //<!--
             function valida(str) {
-                ini = str.indexOf("(");
-                mid = str.indexOf(",");
-                last = str.indexOf(")");
+                var ini = str.indexOf("(");
+                var mid = str.indexOf(",");
+                var last = str.indexOf(")");
                 if (ini < 0 || last < 0) {
                     document.write("Invalido el comando");
                     return -1;
@@ -21,68 +21,62 @@ and open the template in the editor.
                 //OP
                 var op = str.substring(0, ini);
 
-                var a = str.substring(ini + 1, mid);
                 //si hay 2 atributos
                 var res;
                 if (mid != -1) {
+                    var a = str.substring(ini + 1, mid);
                     var b = str.substring(mid + 1, last);
-                    res = [op,a,b];
-                }else{
-                    res = [op,a];
+                    res = [op, a, b];
+                } else {
+
+                    var a = str.substring(ini + 1, last);
+                    res = [op, a];
                 }
+                //si hay 1 
                 return res;
-                
+
             }
             function trata(str, cad) {
-                ini = str.indexOf("(");
-                mid = str.indexOf(",");
-                last = str.indexOf(")");
-                if (ini < 0 || last < 0) {
-                    document.write("Invalido el comando");
-                    return -1;
-                }
-                //OP
-                var op = str.substring(0, ini);
-
-                var a = str.substring(ini + 1, mid);
-                //si hay 2 atributos
-                if (mid >= 0) {
-                    var b = str.substring(mid + 1, last);
+                var op = str[0];
+                var a = str[1];
+                //comprueba si hay 2 atributos
+                if (str.length > 2) {
+                    var b = str[2];
                 }
                 switch (op) {
-                    //devuelve subcadena de cad desde  i a j
                     case "sub":
-                        inta = parseInt(a);
-                        intb = parseInt(b);
-                        //comprobamos que sean numeros
-                        if (!isNaN(inta) && !isNaN(intb)) {
-                            if (inta > intb || intb > cad.length || inta < 0) {
-                                return -1;
-                            }
-                        } else {
-                            return -1;
-                        }
-                        return cad.substring(inta, intb + 1);
-
-
+                        return sub(cad,a,b);
                         break
-                        //unico parametro
-                        //concatena a al final de cad
                     case "cat":
-                        if (mid >= 0) {
-                            return -1;
-                        }
-                        //TRABAJA
-                        return cad.concat(a);
+                        return cat(cad,a);
                         break;
-                        //remplaza en cad a por b
                     case "rep":
-                        return cad.replace(a, b);
+                        return rep(cad,a,b);
                         break;
                     default:
                         return "No valida op";
                         break;
                 }
+            }
+            function sub(cad, a, b) {
+                var inta = parseInt(a);
+                var intb = parseInt(b);
+                //comprobamos que sean numeros
+                if (!isNaN(inta) && !isNaN(intb)) {
+                    if (inta > intb || intb > cad.length || inta < 0) {
+                        return -1;
+                    }
+                } else {
+                    return -1;
+                }
+                return cad.substring(inta - 1, intb + 1);
+            }
+            function cat(cad, a) {
+                return cad.concat(a);
+            }
+            function rep(cad, a, b) {
+                var regex = new RegExp(a, "g");
+                return cad.replace(regex, b);
             }
             //-->
         </script>
@@ -90,18 +84,21 @@ and open the template in the editor.
     <body>
         <script type="text/javascript">
 //<!--
-//Cadena = "cad;op(a,b        );opb(a,b)"
-            var cadena = prompt("Envia la operacion:", "");
-            var cadArr = cadena.split(";");
-            var cad = cadArr[0];
+            //Cadena = "cad;op(a,b);opb(a,b)"
+            cadena = prompt("Envia la operacion:", "");
+            cadArr = cadena.split(";");
+            cad = cadArr[0];
+            end = "";
+            //realizamos cada operacion
             for (var i = 1; i < cadArr.length; i++) {
+                //cadArr[i] = ['op(a,b)`] o [op(a)]
                 operandos = valida(cadArr[i]);
-                var res = trata(cadArr[i], cad);
+                res = trata(operandos, cad);
+                cad = res;
                 if (res == -1) {
                     alert("error");
                 } else {
-                    res.concat("<br>");
-                    document.write(res);
+                    document.write(" " + res);
                 }
             }
             if (cadArr.length < 2) {

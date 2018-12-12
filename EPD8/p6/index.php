723e2170c12a638e0d52111e774fc5e579d93a88
Kazzpa <andres97cb@hotmail.com>
2018-12-12 15:00:02 +0100

    EPD8-P6

---
+++ EPD8/p6/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>EPD8-P6</title>
+        <script type="text/javascript">
+<!--
+
+            //-->
+        </script>
+    </head>
+    <body>
+        <form action="" method="POST">
+            Edad :<input type="number" name="edad"><br/>
+            Sexo:
+            &nbsp;<input type="radio" name="gender" value="male" checked> hombre<br/>
+            &nbsp;<input type="radio" name="gender" value="female"> muhe<br/>
+            &nbsp;<input type="radio" name="gender" value="other"> otro
+        </form>
+        <script type="text/javascript">
+<!--
+
+
+//-->
+        </script>
+        <?php
+        // put your code here
+        ?><footer>
+            <p>Posted by grupo 4:<br /> Javier Bermejo Torrent, Andr&eacute;s Carrillo Bejarano, Ander Lakidain de Arriba</p>
+        </footer>
+    </body>
+</html>
